280abdad48d1ecae84c02f7b6c5b2f8286453285
hyunjoebrother <hyunjoe31@gmail.com>
2021-03-04 16:16:15 +0900

    210304_VanillaJS_study

--- 실습/index.js
+++ 실습/index.js
@@ -1,4 +1,29 @@
 // JS 파일은 항상 body 태그 밑에 있어야 함
 
-alert('Hello World!');
+//alert('Hello World!');
 //console.log('Hello World!');
+
+/*
+const MeiInfo = {
+    name : "Mei",
+    age : 24,
+    gender : "Female",
+    inHandsome : true
+}
+
+console.log(MeiInfo.age)
+
+// 함수 정의
+function sayHello() {
+    console.log('Hello!');
+}
+*/
+
+// Argument 사용
+function sayHello() {
+    console.log('Hello!', potato);
+}
+
+//sayHello();
+sayHello("Mei");
+console.log("Hi")
