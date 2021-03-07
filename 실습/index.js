bbd391fc57d591ddb238ea310ffb7910f3b06adb
hyunjoebrother <hyunjoe31@gmail.com>
2021-03-07 15:58:45 +0900

    210307_VanillaJS_study

--- 실습/index.js
+++ 실습/index.js
@@ -20,12 +20,35 @@ function sayHello() {
 */
 
 
-// Argument 사용
-function sayHello() {
-    console.log('Hello!', potato);
-}
+// // Argument 사용
+// function sayHello(potato, age) {
+//     console.log('Hello!', potato, "youre", age);
+// }
 
-//sayHello();
-sayHello("Mei");
-console.log("Hi")
+// //sayHello();
+// sayHello("Mei", 17);
+// console.log("Hi")
 
+// function sayHello(name, age)
+// {
+//     //console.log(`Hello ${name} you are ${age} years old`);
+//     return `Hello ${name} you are ${age} years old`;
+// }
+
+// //sayHello("Mei", 17);
+// const greetMei = sayHello("Mei", 17)
+
+// console.log(greetMei)
+
+// const calcualtor = {
+//     plus: function(a, b)
+//     {
+//         return a+b;
+//     }
+// }
+
+// const plus = calcualtor.plus(5, 10)
+// console.log(plus)
+
+
+//// JS에서 id 선택하기
