29141a141bb8e1fdce884875e8507ef1b5f4c057
hyunjoebrother <hyunjoe31@gmail.com>
2021-03-24 23:51:21 +0900

    210324_VanillaJSstudy

--- 실습/clock.js
+++ 실습/clock.js
@@ -9,8 +9,17 @@ function getTime() {
     const seconds = date.getSeconds();
 
     // 객체 안에 text 넣자
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    //clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
     // 하지만 아직 새로고침을 해야함 ㅇㅇ 실시간 반영이 아니다 ㅠㅠ
+
+    // 유사 if문으로 숫자가 00 01 02 나오도록 한다
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours
+    }:${
+        minutes < 10 ? `0${minutes}` : minutes
+    }:${
+        seconds < 10 ? `0${seconds}` : seconds
+    }`;
 }
 
 
@@ -18,5 +27,7 @@ function getTime() {
 function init() {
     // 현재 시각을 얻자
     getTime();
+    setInterval(getTime, 1000); // 시간 간격 1000ms = 1초
 }
+
 init();
