ce1db224097d710bfb12ac0781f00d95f24200e8
hyunjoebrother <hyunjoe31@gmail.com>
2021-03-27 17:14:53 +0900

    210327_VanillaJSstudy

--- 실습/todo.js
+++ 실습/todo.js
@@ -1,5 +1,19 @@
-// 먼저 init 함수 세팅
-function init() {
+// const(상수) 선언할 때 다른 파일이랑 이름 겹쳐서 바꾸자 (js모듈들을 분리시키도록)
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = form.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
 
+
+
+// local storage에서 정보를 가져옴
+function loadToDos() {
+    const current
+
+}
+
+
+// 먼저 init 함수 세팅
+    function init() {
+        loadToDos();
 }
 init();
