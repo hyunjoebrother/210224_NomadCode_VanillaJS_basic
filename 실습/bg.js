5eeaeaed4a371ef281026caecb01af1bea2995a8
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-03 23:04:37 +0900

    210403_VanillaJSstudy

---
+++ 실습/bg.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body");
+
+// 숫자 랜덤 생성 함수를 만들자
+function genRandom() {
+    const number = 
+    return number;
+}
+
+function init() {
+    const randomNumber = 
+}
+init();
