a3bf54e03a61caa4e7ea01de40fc470b8a3f4139
hyunjoebrother <hyunjoe31@gmail.com>
2021-04-05 15:40:19 +0900

    210405_VanillaJSstudy

--- 실습/bg.js
+++ 실습/bg.js
@@ -1,12 +1,26 @@
 const body = document.querySelector("body");
 
+const IMG_NUMBER = 3; // image 갯수
+
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`; // src : images 폴더
+
+    image.classList.add("bgImage"); // Image css 수정하기 위해 class 만들어줌
+
+    body.appendChild(image);
+}
+
+
 // 숫자 랜덤 생성 함수를 만들자
 function genRandom() {
-    const number = 
+    const number = Math.floor(Math.random() * IMG_NUMBER);
     return number;
 }
 
 function init() {
-    const randomNumber = 
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
 }
 init();
